b39cf4739fc8ab3ecd56229b163294256f72e78c
Tom Winterton <tomwintertoniv@gmail.com>
2024-12-02 11:23:50 -0600

    Updated Backend Boilerplate

---
+++ Backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//THIS REQUIRES A .env FILE WITH MONGODB_URI and PORT
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.error('Error connecting to MongoDB', error));
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello from Express and Mongoose!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
