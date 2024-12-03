a8061c4c4fcf0d0124614a1d5e8a821139b4754c
ginah903 <ginah903@gmail.com>
2024-12-03 17:12:41 -0600

    Update Recipe.js

--- Backend/Recipe.js
+++ Backend/Recipe.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
     title: {type: String, required: true},
+    cuisineType: {type: [String]},
     ingredients: {type: [String], required: true},
+    totalPrepTime: {type: Number, required: true},
     totalCookTime: {type: Number, required: true},
     instructions: {type: String, required: true},
     uploadDate: {type: Date, default: Date.now},
