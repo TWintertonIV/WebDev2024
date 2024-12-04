4d4a4c89306929c93fccee622cb7bcb876447088
ginah903 <ginah903@gmail.com>
2024-12-04 15:13:02 -0600

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const recipeController = require('./controllers/recipeController'); // import recipe controller
+const path = require('path'); // ADDED!!!!
 
 //THIS REQUIRES A .env FILE WITH MONGODB_URI and PORT
 
 const dotenv = require('dotenv');
 dotenv.config();
-const Recipe = require('./models/Recipe'); // import recipe schema
+const Recipe = require('./models/Recipe'); // Import recipe schema
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Middleware for POST requests
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
 
 
 // Connect to MongoDB
@@ -19,15 +24,18 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch((error) => console.error('Error connecting to MongoDB', error));
 
+app.use(express.static(path.join(__dirname, 'public'))); // ADDED!!!
+
 // Define a simple route
 app.get('/', (req, res) => {
   res.send('Hello from Express and Mongoose!');
 });
 
-// more routes
+// More routes
 app.get('/homepage', recipeController.homepage);
 app.get('/recipes', recipeController.getAllRecipes);
 app.get('/recipes/:id', recipeController.getRecipeByID);
+app.post('/recipes', recipeController.addRecipe);
 
 // Start the server
 app.listen(PORT, () => {
