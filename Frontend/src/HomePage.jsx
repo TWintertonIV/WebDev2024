2782e308206069385d139d3246410a06cf56d78e
elisefidler <efidler255@gmail.com>
2024-12-05 17:51:57 -0600

    sdfgyhkjhm

--- Frontend/src/HomePage.jsx
+++ Frontend/src/HomePage.jsx
@@ -1,35 +1,57 @@
-import RecipeCard from "./RecipeCard";
+// import RecipeCard from "./RecipeCard";
+import { useState } from "react";
+import { useEffect } from "react";
 
-function HomePage(){
 
-    return(
-        <div class="home-section">
-            <div class="section-content">
-                <div class="home-details">
-                    <h1 class="title">Welcome to your Recipe Hub</h1>
-                    <h3 class="subtitle">Create, Organize, and Share!</h3>
-                    <p class="description">Whether you're a seasoned chef or just getting started, we've got the
-                                            tools you need to make your culinary journey enjoyable and effortless.</p>
-                    
-                    <div class="buttons">
-                        <a href="#" class="button login">Login</a>
+
+function HomePage() {
+    const [recipes, setRecipes] = useState([]);
+    
+    useEffect(() => {
+        fetch('http://localhost:3000/recipes')
+            .then(response => response.json())
+            .then(data => setRecipes(data));
+    }, []);
+    
+    return (
+        <div className="home-section">
+            <div className="section-content">
+                <div className="home-details">
+                    <h1 className="title">Welcome to your Recipe Hub</h1>
+                    <h3 className="subtitle">Create, Organize, and Share!</h3>
+                    <p className="description">Whether you're a seasoned chef or just getting started, we've got the
+                        tools you need to make your culinary journey enjoyable and effortless.</p>
+
+                    <div className="buttons">
+                        <a href="#" className="button login">Login</a>
                     </div>
                 </div>
-                <div class="hero-image-wrapper">
-                    <img src="images/heroImg3.png" alt="Hero" class="hero-image"/>
+                <div className="hero-image-wrapper">
+                    <img src="images/heroImg3.png" alt="Hero" className="hero-image" />
                 </div>
             </div>
 
-            <div class="recipes-section">
-                <div class="recipes-details">
-                    <h2 class="section-title">My Recipes</h2>
-                    <RecipeCard />
+            <div className="recipes-section">
+                <div className="recipes-details">
+                    <h2 className="section-title">My Recipes</h2>
+                    {/* <RecipeCard /> */}
+
+                    {recipes.map((recipe) => (
+                        <div>
+                            <Link to={`/recipes/${recipe._id}`}>
+                                <h2>
+                                    {recipe.title}
+                                </h2>
+                            </Link>
+                        </div>
+                    ))}
+
                 </div>
             </div>
 
         </div>
 
-    );    
+    );
 
 }
 
