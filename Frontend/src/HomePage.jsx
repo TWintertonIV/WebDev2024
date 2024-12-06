5790d732ca10d0d986b17a427783dbef4e91bc14
elisefidler <efidler255@gmail.com>
2024-12-06 15:27:19 -0600

    favicon egg

--- Frontend/src/HomePage.jsx
+++ Frontend/src/HomePage.jsx
@@ -2,6 +2,7 @@
 import './HomePage.css';
 import hero from '../public/images/heroImg3.png';
 import logo from '../public/images/whiteLogo.png';
+import egg from '../public/images/egg.png';
 import { Link } from 'react-router-dom';
 function HomePage(){
 
@@ -11,7 +12,7 @@ function HomePage(){
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Recipe Hub</title>
         {/*favicon: icon infront of tab name*/}
-        <link rel="shortcut icon" href={logo} type="image/x-icon" />
+        <link rel="shortcut icon" href={egg} type="image/x-icon" />
         {/* Header / Navbar */}
         <header>
             <nav className="navbar section-content">
