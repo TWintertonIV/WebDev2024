75f170297ae246d3921ae83f56c85f1ea71d53d7
Amwaj <amwajsabri@gmail.com>
2024-12-06 16:04:43 -0600

    style added

--- Frontend/src/RecipeDetails.jsx
+++ Frontend/src/RecipeDetails.jsx
@@ -1,6 +1,6 @@
 import {Link, useParams} from 'react-router-dom';
 import { FC, useState, useEffect } from 'react';
-
+import './RecipeDetails.css';
 
 function RecipeDetails() {
     const {_id} = useParams();
@@ -26,16 +26,41 @@ function RecipeDetails() {
     }
 
     return (
-        <>
+        <div className="container">
             <h2>{recipe.title}</h2>  
-            <p>Cuisine type: {recipe.cuisineType.join(', ')}</p>
-            <p>Ingredients: {recipe.ingredients.join(', ')}</p>
-            <p>Total prep time: {recipe.totalPrepTime} minutes</p>
-            <p>Total cook time: {recipe.totalCookTime} minutes</p>
-            <p>Instructions: {recipe.instructions}</p>
-            <p>Upload date: {new Date(recipe.uploadDate).toLocaleDateString()}</p>
-            <p><Link to="/Recipes">All Recipes</Link></p>
-        </>
+
+            <div className="recipe-details">
+
+                <div className="food-details">
+                    <h3>Cusine Type</h3>
+                    <p>{recipe.cuisineType.join(', ')}</p>
+                </div>
+                <div className="food-details">
+                    <h3>Total Prep Time: </h3>
+                    <p>{recipe.totalPrepTime} minutes</p>
+                </div>
+                <div className="food-details">
+                    <h3>Total Cook Time: </h3>
+                    <p>{recipe.totalCookTime} minutes</p>
+                </div> 
+            </div>
+
+            <div className="food-description">
+                <h3>Ingredients</h3>
+                <p>{recipe.ingredients.join(', ')}</p>
+            
+                <h3>Instructions</h3>
+                <p>{recipe.instructions}</p>
+            </div>
+
+            <div className="upload-date">
+                <p>Upload date: {new Date(recipe.uploadDate).toLocaleDateString()}</p>
+            </div>
+            
+            <div className="recipes-button">
+                <p><Link to="/Recipes">All Recipes</Link></p>
+            </div>
+        </div>
     );
 }
 
